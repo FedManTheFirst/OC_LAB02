6a144704e1208053b299a4c98e5151028d08c7ca
FedMan(Федя) <fpyrshiev@bk.ru>
2022-03-01 15:49:36 +0300

    1/2

--- OC_LAB02/OC_LAB02.cs
+++ OC_LAB02/OC_LAB02.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Security.Cryptography;
+using System.Threading;
+using System.Diagnostics;
 
 public class OC_LAB02
 {
@@ -8,8 +10,9 @@ public class OC_LAB02
     public delegate bool BruteForceTest(ref char[] testChars);
     public static void Main(string[] args)
     {
+
     mark:
-        Console.WriteLine("1-Работа с файлами, 2-хэширование, 3-ломать, 0-выход: ");
+        Console.WriteLine("1-Работа с файлами, 2-хэширование, 3-ломать, 4 многопоточность, 0-выход: ");
         string choice = Console.ReadLine();
         switch (choice)
         {
@@ -23,6 +26,8 @@ public class OC_LAB02
                 BrutMain();
                 goto mark;
             case "4":
+                Theard();
+                Thread.Sleep(10000);
                 goto mark;
             case "5":
                 goto mark;
@@ -108,49 +113,49 @@ public class OC_LAB02
         Console.ReadLine();
     }
     static void SHA_256()
+    {
+        if (Directory.Exists(path))
         {
-            if (Directory.Exists(path))
+            // Create a DirectoryInfo object representing the specified directory.
+            var dir = new DirectoryInfo(path);
+            // Get the FileInfo objects for every file in the directory.
+            FileInfo[] files = dir.GetFiles();
+            // Initialize a SHA256 hash object.
+            using (SHA256 mySHA256 = SHA256.Create())
             {
-                // Create a DirectoryInfo object representing the specified directory.
-                var dir = new DirectoryInfo(path);
-                // Get the FileInfo objects for every file in the directory.
-                FileInfo[] files = dir.GetFiles();
-                // Initialize a SHA256 hash object.
-                using (SHA256 mySHA256 = SHA256.Create())
+                // Compute and print the hash values for each file in directory.
+                foreach (FileInfo fInfo in files)
                 {
-                    // Compute and print the hash values for each file in directory.
-                    foreach (FileInfo fInfo in files)
+                    using (FileStream fileStream = fInfo.Open(FileMode.Open))
                     {
-                        using (FileStream fileStream = fInfo.Open(FileMode.Open))
+                        try
                         {
-                            try
-                            {
-                                // Create a fileStream for the file.
-                                // Be sure it's positioned to the beginning of the stream.
-                                fileStream.Position = 0;
-                                // Compute the hash of the fileStream.
-                                byte[] hashValue = mySHA256.ComputeHash(fileStream);
-                                // Write the name and hash value of the file to the console.
-                                Console.Write($"{fInfo.Name}: ");
-                                PrintByteArray(hashValue);
-                            }
-                            catch (IOException e)
-                            {
-                                Console.WriteLine($"I/O Exception: {e.Message}");
-                            }
-                            catch (UnauthorizedAccessException e)
-                            {
-                                Console.WriteLine($"Access Exception: {e.Message}");
-                            }
+                            // Create a fileStream for the file.
+                            // Be sure it's positioned to the beginning of the stream.
+                            fileStream.Position = 0;
+                            // Compute the hash of the fileStream.
+                            byte[] hashValue = mySHA256.ComputeHash(fileStream);
+                            // Write the name and hash value of the file to the console.
+                            Console.Write($"{fInfo.Name}: ");
+                            PrintByteArray(hashValue);
+                        }
+                        catch (IOException e)
+                        {
+                            Console.WriteLine($"I/O Exception: {e.Message}");
+                        }
+                        catch (UnauthorizedAccessException e)
+                        {
+                            Console.WriteLine($"Access Exception: {e.Message}");
                         }
                     }
                 }
             }
-            else
-            {
-                Console.WriteLine("The directory specified could not be found.");
-            }
         }
+        else
+        {
+            Console.WriteLine("The directory specified could not be found.");
+        }
+    }
     // Display the byte array in a readable format.
     static void PrintByteArray(byte[] array)
     {
@@ -205,7 +210,7 @@ public class OC_LAB02
     }
     static void BrutMain()
     {
-        mark:
+    mark:
         Console.WriteLine("ввод: ");
         string choice = Console.ReadLine();
         if (choice.Length <= 5)
@@ -228,4 +233,41 @@ public class OC_LAB02
 
 
     }
+    static void Theard()
+    {
+        object locker = new();
+        Stopwatch stopWatch = new Stopwatch();
+        Console.WriteLine("Колл-во потоков:");
+        int theards = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Дождитесь меню!");
+        for (int i = 1; i <= theards; i++)
+        {
+            stopWatch.Start();
+            Thread myThread = new(Print);
+            myThread.Name = $"Поток {i}";   // устанавливаем имя для каждого потока
+            myThread.Start();
+        }
+        // действия, выполняемые во втором потокке
+        void Print()
+        {
+            lock (locker)
+            {
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread currentThread = Thread.CurrentThread;
+                    Console.WriteLine($"{Thread.CurrentThread.Name}: {i}");
+
+                }
+                stopWatch.Stop();
+                TimeSpan ts = stopWatch.Elapsed;
+                string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+                Console.WriteLine("RunTime " + elapsedTime);
+            }
+
+        }
+    }
 }
+
